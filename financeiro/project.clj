092bd51883b8e1fefb131c94b95e0baf25bbf427
matheusnajal <matheusnajalk@gmail.com>
2024-05-23 17:55:17 -0300

    sistema financeiro

--- financeiro/project.clj
+++ financeiro/project.clj
@@ -2,11 +2,14 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [compojure "1.6.0"]
-                 [ring/ring-defaults "0.3.1"]
+  :uberjar-name "financeiro.jar"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [compojure "1.6.1"]
+                 [cheshire "5.8.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-json "0.4.0"]
                  [clj-http "3.9.1"]]
-  :plugins [[lein-ring "0.12.5"]]
+  :plugins [[lein-ring "0.12.4"]]
   :ring {:handler financeiro.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
@@ -14,5 +17,6 @@
                         [midje "1.9.6"]
                         [ring/ring-core "1.7.1"]
                         [ring/ring-jetty-adapter "1.7.1"]]
-        :plugins [[lein-midje "3.2.1"]
-                  [lein-cloverage "1.0.13"]]}})
+         :plugins [[lein-midje "3.2.1"]
+                   [lein-cloverage "1.0.13"]]}}
+  :test-paths ["test/unitarios" "test/aceitacao"])
