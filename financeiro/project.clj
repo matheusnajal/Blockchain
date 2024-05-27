7f7864c0518d30dd2dcbd99900159ff4ee20133a
matheusnajal <matheusnajalk@gmail.com>
2024-05-27 16:00:16 -0300

    consertando

--- financeiro/project.clj
+++ financeiro/project.clj
@@ -2,14 +2,11 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :uberjar-name "financeiro.jar"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
-                 [cheshire "5.8.1"]
                  [ring/ring-defaults "0.3.2"]
-                 [ring/ring-json "0.4.0"]
                  [clj-http "3.9.1"]]
-  :plugins [[lein-ring "0.12.4"]]
+  :plugins [[lein-ring "0.12.5"]]
   :ring {:handler financeiro.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
--- financeiro/src/financeiro/transacoes.clj
+++
@@ -1,9 +0,0 @@
-(ns financeiro.transacoes)
-
-(defn valida? [transacao]
-  (and (contains? transacao :valor)
-       (number? (:valor transacao))
-       (pos? (:valor transacao))
-       (contains? transacao :tipo)
-       (or (= "despesa" (:tipo transacao))
-           (= "receita" (:tipo transacao)))))
