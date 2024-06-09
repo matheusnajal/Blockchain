a515a88941e03191c2710c1f0c2a16345fff5930
ludihan <65617704+ludihan@users.noreply.github.com>
2024-06-09 20:16:46 -0300

    aff

---
+++ client/src/client/ops.clj
@@ -0,0 +1,41 @@
+(ns client.ops
+  (:require [cheshire.core :as json]
+            [clojure.pprint :as pprint]
+            [clj-http.client :as client]
+            [clojure.string :as string]
+            ))
+
+(defn- post-thing [endpoint body]
+  (-> (str "http://localhost:3000/" endpoint)
+      (client/post { :content-type :json
+                     :body body})))
+
+(defn- get-thing [endpoint]
+  (-> (str "http://localhost:3000/" endpoint)
+      (client/get)
+      (:body)
+      (json/parse-string)))
+
+(defn show-transaction []
+  (let [transacoes (get (get-thing "transacoes") "transacoes")]
+    (if (seq transacoes)
+      (pprint/print-table (get (get-thing "transacoes") "transacoes"))
+      (println "Nenhuma transação registrada"))))
+
+(defn create-transaction []
+  (println "valor e tipo")
+  (let [input (string/split (read-line) #" ")]
+    (println input)
+    (println (json/generate-string
+               {:valor (get input 0)
+                :tipo (get input 1) }))
+    (if (not (< (count input) 2))
+      (post-thing "transacoes" (json/generate-string
+                                 {:valor (get input 0)
+                                  :tipo (get input 1) }))
+      (recur)))
+  )
+
+(defn show-blockchain []
+  (get-thing "blockchain"))
+
